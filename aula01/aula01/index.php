6b87b6ea4595127a2fec8380775d9dc382d1f125
thekaiofc <kaio.hen13@gmail.com>
2023-03-14 19:50:08 -0300

    primeira aula de PWII

---
+++ aula01/aula01/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá mundo!</h1>
+
+    <?php
+
+        $nome="Kaio";
+        $idade= 19;
+        $peso = 80.7;
+
+        echo $nome . "<br />";
+        echo $idade . "<br />";
+        echo $peso . "<br />";
+
+        echo "Kaio Henrique";
+
+    ?>
+</body>
+</html>
